7f8fc4c4d5f0f3a0d41c171064a1759d4a3aa949
BlackEel <3513924511@qq.com>
2024-04-09 22:50:16 +0800

    .

---
+++ 第二节课作业1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+using namespace std;
+char getRandomChar() {
+    return char(rand() % 26 + 'A');
+}
+
+int main() {
+    srand(static_cast<unsigned int>(time(0)));
+    char correctChar = getRandomChar();
+    char userInput;
+
+    cout << "ÊäÈë×ÖÄ¸ ";
+    cout << correctChar << endl;
+
+    while (true) {
+        cout << "ÇëÊäÈë: ";
+        cin >> userInput;
+
+        if (toupper(userInput) == toupper(correctChar)) {
+            cout << "ÕýÈ· " << correctChar << endl;
+            break;
+        }
+        else {
+            cout << "´íÎó " << correctChar << endl;
+        }
+    }
+
+    return 0;
+}
